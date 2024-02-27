2ee42d226f09354c8079a02b4db0086acfe3dda9
Ayush Singh <asayushsingh80@gmail.com>
2024-02-28 00:52:17 +0530

    refactored homepage and added features component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,13 @@
 "use client";
-import { LampContainer } from "@/components/ui/lamp";
-import { motion } from "framer-motion";
 
-import { Titan_One } from "next/font/google";
-const titanOne = Titan_One({ subsets: ["latin"], weight: "400" });
+import Features from "@/components/features";
+import Welcome from "@/components/welcome";
 
 export default function Home() {
   return (
-    <motion.div>
-      <LampContainer>
-        <motion.h1
-          initial={{ opacity: 0.5, y: 160 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className={`${titanOne.className} mt-8 bg-gradient-to-b from-rose-300 to-white py-4 bg-clip-text text-center text-4xl font-medium text-transparent md:text-7xl tracking-wide`}
-        >
-          Health Club <br />
-          <span className="text-3xl text-rose-200 tracking-wider">
-            Where Ideas Heal, Together
-          </span>
-        </motion.h1>
-      </LampContainer>
-    </motion.div>
+    <>
+      <Welcome />
+      <Features />
+    </>
   );
 }
---
+++ src/components/welcome.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { LampContainer } from "@/components/ui/lamp";
+import { motion } from "framer-motion";
+
+import { Titan_One } from "next/font/google";
+const titanOne = Titan_One({ subsets: ["latin"], weight: "400" });
+export default function Welcome() {
+  return (
+    <section className="min-h-screen w-full">
+      <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 160 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className={`${titanOne.className} mt-8 bg-gradient-to-b from-rose-300 to-white py-4 bg-clip-text text-center text-4xl font-medium text-transparent md:text-7xl tracking-wide`}
+        >
+          Health Club <br />
+          <span className="text-3xl text-rose-200 tracking-wider">
+            Where Ideas Heal, Together
+          </span>
+        </motion.h1>
+      </LampContainer>
+    </section>
+  );
+}
