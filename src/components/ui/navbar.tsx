49097d357972c4c7579468520cb79436dfaf22f6
Ayush Singh <asayushsingh80@gmail.com>
2024-02-28 02:09:53 +0530

    basic navbar layout

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import { Button } from "./button";
+import Image from "next/image";
+
 export default function Navbar() {
   return (
-    <div className="w-full flex justify-center fixed top-0 z-1000 bg-[rgba(12, 10, 9, 1,0.6)] backdrop-blur-md">
-      <div className="flex items-center">hello</div>
+    <div className="w-full fixed top-0 z-1000 p-2 backdrop-blur-md">
+      <div className="flex justify-between items-center max-w-[1170px] mx-auto w-full">
+        <div className="">logo aayega</div>
+        <div>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/login"}>Login</Link>
+          </Button>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/signup"}>Sign Up</Link>
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
