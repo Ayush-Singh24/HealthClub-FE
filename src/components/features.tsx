e134e02171f00152e48709322cbe341893215b66
Ayush Singh <asayushsingh80@gmail.com>
2024-02-28 20:15:35 +0530

    implemented cards with hovereffect componenets

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,8 +1,29 @@
 import Image from "next/image";
 import { Titan_One } from "next/font/google";
+import { HoverEffect } from "./ui/hoverEffect";
 const titanOne = Titan_One({ subsets: ["latin"], weight: "400" });
 
 export default function Features() {
+  const projects = [
+    {
+      title: "Stripe",
+      description:
+        "A technology company that builds economic infrastructure for the internet.",
+      link: "https://stripe.com",
+    },
+    {
+      title: "Netflix",
+      description:
+        "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+      link: "https://netflix.com",
+    },
+    {
+      title: "Google",
+      description:
+        "A multinational technology company that specializes in Internet-related services and products.",
+      link: "https://google.com",
+    },
+  ];
   return (
     <section className="w-full min-h-screen flex flex-col items-center gap-32">
       <div className="flex flex-col justify-center items-center gap-2 text-center">
@@ -25,6 +46,9 @@ export default function Features() {
           width={500}
           height={500}
         />
+      </div>
+      <div className="max-w-[1170px]">
+        <HoverEffect items={projects} />
       </div>
     </section>
   );
