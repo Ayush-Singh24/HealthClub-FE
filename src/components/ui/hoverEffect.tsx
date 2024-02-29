4d6b40c02fd8a387d543276f17c4bcc990aee994
Ayush Singh <asayushsingh80@gmail.com>
2024-03-01 00:02:20 +0530

    adjusted z-indexes of card components

--- src/components/ui/hoverEffect.tsx
+++ src/components/ui/hoverEffect.tsx
@@ -33,7 +33,7 @@ export const HoverEffect = ({
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-neutral-200 dark:bg-slate-800/[0.8] block  rounded-3xl -z-20"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
@@ -70,7 +70,7 @@ export const Card = ({
   return (
     <div
       className={cn(
-        "rounded-2xl h-full w-full p-3 overflow-hidden bg-rose-50 dark:bg-sdColor border border-black/[0.2] dark:border-white/[0.2] group-hover:border-slate-700 relative z-20",
+        "rounded-2xl h-full w-full p-3 overflow-hidden bg-rose-50 dark:bg-sdColor border border-black/[0.2] dark:border-white/[0.2] group-hover:border-slate-700 relative -z-10",
         className
       )}
     >
