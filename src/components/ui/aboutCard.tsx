a3de27b6707a7098c35836c539f840370731a694
Ayush Singh <asayushsingh80@gmail.com>
2024-03-11 00:57:24 +0530

    removed uneccesary imports

--- src/components/ui/aboutCard.tsx
+++ src/components/ui/aboutCard.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 
 export const AboutCard = ({
   items,
