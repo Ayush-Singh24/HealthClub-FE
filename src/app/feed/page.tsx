347f35a82b6f6657f6b73e7cf3c75c874f91f12f
Ayush Singh <asayushsingh80@gmail.com>
2024-03-10 06:08:13 +0530

    implemented authorization check in feed page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useAuth } from "@/hooks/useAuth";
+import Loader from "../loader";
+import { useRouter } from "next/navigation";
+
 export default function Feed() {
+  const isAuth = useAuth();
+  const router = useRouter();
+  if (isAuth === null) {
+    return <Loader />;
+  }
+  if (!isAuth) {
+    router.push("/home");
+    return <Loader />;
+  }
+
   return <div>Hello feed</div>;
 }
