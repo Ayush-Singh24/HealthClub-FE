ed5dcb69b14ed080c991900c73b0bf88b09df7f9
Ayush Singh <asayushsingh80@gmail.com>
2024-03-13 18:17:38 +0530

    sidebar ui changes

--- src/app/loader.tsx
+++ src/app/loader.tsx
@@ -2,7 +2,7 @@
 import { Comment } from "react-loader-spinner";
 export default function Loader() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex justify-center items-center">
       <Comment
         visible={true}
         height={200}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -5,6 +5,7 @@ import {
   Mail,
   Plus,
   Search,
+  SunMoon,
   UserRound,
   UsersRound,
 } from "lucide-react";
@@ -14,15 +15,24 @@ import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
 
 export default function SideBar() {
-  const theme = useTheme();
   const pathname = usePathname();
+  const { theme, setTheme } = useTheme();
+  const handleTheme = () => {
+    theme === "dark" ? setTheme("light") : setTheme("dark");
+  };
   return (
     <div className="hidden h-screen lg:basis-1/4 md:flex flex-col overflow-hidden sticky left-0 top-0">
       <div className="py-5 px-7 hidden lg:block">
-        <img src={`/images/logo-light.png`} className="w-36" />
+        <img src="/images/logo-light.png" className="w-36 hidden dark:block" />
+        <img src="/images/logo-dark.png" className="w-36 block dark:hidden" />
       </div>
-      <div className="flex flex-col p-5 gap-8 justify-center items-stretch h-full">
-        <Button className="text-2xl px-0" variant={"ghost"}>
+      <div className="flex flex-col p-5 gap-8 lg:justify-center items-stretch h-full">
+        <Button
+          className={`text-2xl px-0 lg:border-0 ${
+            pathname === "/feed" ? "border-rose-500" : ""
+          } border-2`}
+          variant={"ghost"}
+        >
           <Link
             className="flex justify-center lg:justify-stretch items-center gap-3 w-full p-2"
             href="/feed"
@@ -37,7 +47,14 @@ export default function SideBar() {
             </span>
           </Link>
         </Button>
-        <Button className="text-2xl  px-0" variant={"ghost"}>
+        <Button
+          className={`text-2xl px-0 lg:border-0 ${
+            pathname === "/feed/explore"
+              ? "border-rose-500 border-2"
+              : "border-0"
+          }`}
+          variant={"ghost"}
+        >
           <Link
             href="/feed/explore"
             className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full"
@@ -45,29 +62,43 @@ export default function SideBar() {
             <Search size={30} />
             <span
               className={`${
-                pathname === "/feed" ? `font-semibold` : ""
+                pathname === "/feed/explore" ? `font-semibold` : ""
               } hidden lg:block`}
             >
               Explore
             </span>
           </Link>
         </Button>
-        <Button className="text-2xl px-0" variant={"ghost"}>
+        <Button
+          className={`text-2xl px-0 lg:border-0 ${
+            pathname === "/feed/notifications"
+              ? "border-rose-500 border-2"
+              : "border-0"
+          }`}
+          variant={"ghost"}
+        >
           <Link
-            href="/feed/notification"
+            href="/feed/notifications"
             className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full"
           >
             <Bell size={30} />
             <span
               className={`${
-                pathname === "/feed" ? `font-semibold` : ""
+                pathname === "/feed/notifications" ? `font-semibold` : ""
               } hidden lg:block`}
             >
               Notifications
             </span>
           </Link>
         </Button>
-        <Button className="text-2xl px-0" variant={"ghost"}>
+        <Button
+          className={`text-2xl px-0 lg:border-0 ${
+            pathname === "/feed/messages"
+              ? "border-rose-500 border-2"
+              : "border-0"
+          }`}
+          variant={"ghost"}
+        >
           <Link
             href="/feed/messages"
             className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full"
@@ -75,29 +106,31 @@ export default function SideBar() {
             <Mail size={30} />
             <span
               className={`${
-                pathname === "/feed" ? `font-semibold` : ""
+                pathname === "/feed/messages" ? `font-semibold` : ""
               } hidden lg:block`}
             >
               Messages
             </span>
           </Link>
         </Button>
-        <Button className="text-2xl px-0" variant={"ghost"}>
-          <Link
-            href="/feed/communites"
-            className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full"
-          >
-            <UsersRound size={30} />
-            <span
-              className={`${
-                pathname === "/feed" ? `font-semibold` : ""
-              } hidden lg:block`}
-            >
-              Communities
-            </span>
-          </Link>
+        <Button
+          className="text-2xl px-0"
+          variant={"ghost"}
+          onClick={handleTheme}
+        >
+          <div className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full">
+            <SunMoon size={30} />
+            <span className="hidden lg:block">Theme</span>
+          </div>
         </Button>
-        <Button className="text-2xl px-0" variant={"ghost"}>
+        <Button
+          className={`text-2xl px-0 lg:border-0 ${
+            pathname === "/feed/profile"
+              ? "border-2 border-rose-500"
+              : "border-0"
+          }`}
+          variant={"ghost"}
+        >
           <Link
             href="/feed/profile"
             className="flex justify-center lg:justify-stretch items-center gap-3 p-2 w-full"
@@ -105,18 +138,18 @@ export default function SideBar() {
             <UserRound size={30} />
             <span
               className={`${
-                pathname === "/feed" ? `font-semibold` : ""
+                pathname === "/feed/profile" ? `font-semibold` : ""
               } hidden lg:block`}
             >
               Profile
             </span>
           </Link>
         </Button>
-        <Button className="text-4xl self-start lg:self-center">
-          <div className="rounded-full">
+        <Button className="text-4xl self-start lg:self-center rounded-full flex justify-center items-center lg:p-8">
+          <div className="lg:hidden">
             <Plus />
           </div>
-          <div className="hidden lg:block w-1/2 py-8">Post</div>
+          <div className="hidden lg:block">Post</div>
         </Button>
       </div>
     </div>
